e954c29e5f44a97f46a381291d99daee3b4ae300
Spydr <58859306+Spydr06@users.noreply.github.com>
2022-06-08 21:49:07 +0200

    Update server.py

--- tools/server.py
+++ tools/server.py
@@ -3,7 +3,12 @@ import http.server
 import socketserver
 
 PORT = 3001
+
 Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map.update({
+      ".js": "application/javascript",
+});
+
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
 httpd.serve_forever()
